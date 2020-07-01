1b485fbd3fe5ab314e35428d86c08ec17db39979
saianurag2 <varanasi.saianurag2@gmail.com>
2020-07-01 13:34:13 +0530

    June Day 2

---
+++ TwoCitySchedule.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class TwoCitySchedule {
+    public static int twoCitySchedCost(int[][] costs) {
+        int len = costs.length;
+        Integer[] index = new Integer[len];
+        int[] diff = new int[len];
+        for(int i =0; i<len; i++){
+            index[i]= i;
+            diff[i] = costs[i][0] - costs[i][1];
+        }
+        Arrays.sort(index, new Comparator<Integer> () {
+            public int compare (Integer a, Integer b) {
+                return Integer.compare(diff[b],diff[a]);
+            }
+        });
+        int minSum = 0;
+        int halfPoint = len/2;
+        for(int j = 0; j<len; j++){
+            if(j < halfPoint)
+                minSum = minSum + costs[index[j]][1];
+            else
+                minSum = minSum + costs[index[j]][0];
+        }
+        return minSum;
+    }
+
+    public static void main(String[] args){
+        int[][] costs = {{10,20},{30,200},{400,50},{30,20}};
+        int op = twoCitySchedCost(costs);
+        System.out.println(op);
+    }
+}
